38dd1ed61595cfceac15686cc3dcada82285a887
rana1365 <rana135105@gmail.com>
2025-06-30 03:16:20 +0200

    a movie show booking application started and frontend completed

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import { Toaster } from "react-hot-toast";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import Favorite from "./pages/Favorite";
+import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
+import MyBookings from "./pages/MyBookings";
+import SeatLayout from "./pages/SeatLayout";
+
+const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
+  return (
+    <>
+      <Toaster />
+      {!isAdminRoute && <NavBar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
+        <Route path="/movies/:id/:date" element={<SeatLayout />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+};
+
+export default App;
